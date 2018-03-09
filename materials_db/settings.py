42a28015580341efe95ec64e4fa63c8bdacc60c8
agaiduk <a.gaiduk@gmail.com>
2018-03-09 09:16:32 -0800

    Added urlparse library to Pipfile

--- materials_db/settings.py
+++ materials_db/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-from urlparse import urlparse
+from urllib.parse import urlparse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
